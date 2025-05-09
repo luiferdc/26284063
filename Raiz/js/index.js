c9432f7994ab99d23c112d4572319ccf8fb36bfc
luiferdc <luiferroda@gmail.com>
2025-05-08 23:23:02 -0400

    listado de estudiantes dinamicos usando js

--- Raiz/js/index.js
+++ Raiz/js/index.js
@@ -1,4 +1,5 @@
 import {config} from '../conf/configES.js';
+import {perfiles} from '../datos/index.js'
 
 document.getElementById('ati').textContent = config.sitio[0];
 document.getElementById('ucv').textContent = config.sitio[1];
@@ -6,3 +7,19 @@ document.getElementById('periodo').textContent = config.sitio[2];
 document.getElementById('saludo').textContent = config.saludo + ", Luis Fernando...";
 document.getElementById('botonBuscar').textContent = config.buscar;
 document.getElementById('footer').textContent = config.copyRight;
+
+const lista = document.getElementById('lista-estudiantes');
+
+perfiles.forEach(perfil => {
+    const li = document.createElement('li');
+    const foto = document.createElement('img');
+    const nombre = document.createElement('p');
+    li.className = 'card';
+
+    nombre.textContent = perfil.nombre
+    foto.src="../datos/"+perfil.imagen;
+
+    li.appendChild(foto);
+    li.append(nombre);
+    lista.appendChild(li);
+})
