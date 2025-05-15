2a407799abd768bf2df9d45c6ee06205a2b97881
luiferdc <luiferroda@gmail.com>
2025-05-14 21:19:49 -0400

    configuracion de index.html dinamico y listado de estudiantes dinamicos usando fetch y los json en sintaxis correcta (quitandole el codigo JS que tenian al principio)

--- Raiz/js/index.js
+++ Raiz/js/index.js
@@ -1,13 +1,54 @@
-import {config} from '../conf/configES.js';
-import {perfiles} from '../datos/index.js'
 
-document.getElementById('ati').textContent = config.sitio[0];
-document.getElementById('ucv').textContent = config.sitio[1];
-document.getElementById('periodo').textContent = config.sitio[2];
-document.getElementById('saludo').textContent = config.saludo + ", Luis Fernando...";
-document.getElementById('botonBuscar').textContent = config.buscar;
-document.getElementById('footer').textContent = config.copyRight;
+// cargando configuracion
+fetch("../conf/configES.json")
+    .then(response => {
+        if(!response.ok){
+            throw new Error("No se pudo cargar la configuración");
+        }
+        return response.json();
+    })
+    .then(config => {
+        document.getElementById('ati').textContent = config.config.sitio[0];
+        document.getElementById('ucv').textContent = config.config.sitio[1];
+        document.getElementById('periodo').textContent = config.config.sitio[2];
+        
+        document.getElementById('saludo').textContent = config.config.saludo + ", Luis Fernando...";
+        document.getElementById('botonBuscar').textContent = config.config.buscar;
+        document.getElementById('footer').textContent = config.config.copyRight;
+        
+    })
+    .catch(error => {
+        console.error("Error al cargar la configuración: ", error);
+    })
 
+//cargando listado de estudiantes
+fetch("../datos/index.json")
+    .then(response => {
+        if(!response.ok){
+            throw new Error("No se pudo cargar el listados de los estudiantes");
+        }
+        return response.json();
+    })
+
+    .then(datos => {
+        const lista = document.getElementById('lista-estudiantes');
+        datos.perfiles.forEach(perfil => {
+            const li = document.createElement('li');
+            const foto = document.createElement('img');
+            const nombre = document.createElement('p');
+            li.className = 'card';
+
+            nombre.textContent = perfil.nombre;
+            foto.src="../datos/"+perfil.imagen;
+
+            li.appendChild(foto);
+            li.appendChild(nombre);
+            lista.appendChild(li);
+        })
+    })
+
+
+/*
 const lista = document.getElementById('lista-estudiantes');
 
 perfiles.forEach(perfil => {
@@ -23,3 +64,4 @@ perfiles.forEach(perfil => {
     li.append(nombre);
     lista.appendChild(li);
 })
+*/
