b7a576842957dfcea0736a181747e7d8451892a5
luiferdc <luiferroda@gmail.com>
2025-05-15 02:13:56 -0400

    perfil con informacion dinamica (el parametro del url debe ser ci <perfil.html?ci=26284063> por ejemplo)

---
+++ Raiz/js/perfil.js
@@ -0,0 +1,53 @@
+//Configurando el perfil.html
+fetch("../conf/configES.json")
+    .then(response => response.json())
+    .then(config => {
+        document.getElementById('color').textContent = config.config.color;
+        document.getElementById('libro').textContent = config.config.libro;
+        document.getElementById('musica').textContent = config.config.musica;
+        document.getElementById('videojuego').textContent = config.config.video_juego;
+        document.getElementById('lenguajes').textContent = config.config.lenguajes;
+        document.getElementById('email').textContent = config.config.email;
+    })
+
+
+
+// Cargando la informacion del perfil del estudiante    
+const params = new URLSearchParams(window.location.search);
+const perfilCi = params.get("ci");
+
+let ruta_foto = "";
+
+// cargando ruta de foto
+fetch("../datos/index.json")
+    .then(response => response.json())
+    .then(config => {
+       const perfil = config.perfiles.find(p => p.ci===perfilCi)
+       ruta_foto = perfil.imagen;
+       //console.log(ruta_foto);
+    })
+
+if(perfilCi){
+    const url = "../datos/"+perfilCi+"/perfil.json";
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(perfil =>{
+            document.getElementById('nombre').textContent = perfil.perfil.nombre;
+            document.getElementById('foto').src = "../datos/" + ruta_foto;
+            document.getElementById('descripcion').textContent = perfil.perfil.descripcion;
+            document.getElementById('color_v').textContent = perfil.perfil.color;
+            document.getElementById('libro_v').textContent = perfil.perfil.libro;
+            document.getElementById('musica_v').textContent = perfil.perfil.musica;
+            document.getElementById('videojuego_v').textContent = perfil.perfil.video_juego;
+            document.getElementById('lenguajes_v').textContent = perfil.perfil.lenguajes;
+            document.getElementById('email_v').textContent = perfil.perfil.email;
+        })
+        .catch(error => {
+            console.error("Error al cargar el perfil:",error);
+            document.body.innerHTML = "<h2>Error al cargar el perfil, la cedula introducida no se encuentra en los datos</h2>"
+        })
+
+}else{
+    document.body.innerHTML = "<h2>No se especifico ninguna cedula como parametro en el URL</h2>"
+}
